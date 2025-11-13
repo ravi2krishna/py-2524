bd29136f680f6d13353d616d531a367dd817febc
ravi2krishna <ravi2krishna@gmail.com>
2025-11-13 12:29:37 +0530

    13th Nov - Identifiers & Variables

---
+++ 01_identifiers/demo.py
@@ -0,0 +1,43 @@
+# This is comment
+# function() -> does a task
+# print() is a function to print data
+print(10)
+print(10.5)
+
+# print(ten) # NameError: name 'ten' is not defined. Did you mean: 'len'?
+print("ten")
+print('ten point five')
+print('im listening to the class')
+
+# Identifiers 
+today = "thursday"
+print(today)
+
+# starts with digit
+# 2day = "thursday" # SyntaxError: invalid decimal literal
+# print(2day)
+
+day2moro = "friday"
+print(day2moro)
+
+_2moro = "friday"
+print(_2moro)
+
+# starts with $ only _ allowed
+# $today = "thursday"
+# print($today)
+
+# Improper way of using identifiers 
+x = "Ravi"
+y = 20
+z = 9.5
+
+# Proper way of using identifiers 
+student_name = "Ravi"
+student_age = 20
+student_gpa = 9.5
+
+# Static Data i.e FIXED data 
+STUDENT_AADHAR_ID = 29292992929292
+
+
---
+++ 02_variables/demo.py
@@ -0,0 +1,24 @@
+# Variables 
+
+# Assign Data (Store Data)
+student_name = "Ravi" # string data 
+student_age = 20 # int data 
+student_gpa = 9.5 # float data 
+student_passed = True # boolean data 
+student_aadhar = None # no data 
+
+# Retrieve Data (Get Data) -> use print()
+print(student_name)
+print(student_age)
+print(student_gpa)
+print(student_passed)
+print(student_aadhar)
+
+# Retrieve Data (Get Data) -> use print()
+print("Student Name is: student_name")
+print("Student Name is: " +student_name)
+# print("Student Age is: " +student_age) # TypeError: can only concatenate str (not "int") to str
+print("Student Age is: " ,student_age)
+print("Student GPA is: " ,student_gpa)
+print("Student Passed: " ,student_passed)
+print("Student Aadhar ID: " ,student_aadhar) 
