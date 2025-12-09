26c1d98db69910996f429b8db579212c6ad7b28f
ravi2krishna <ravi2krishna@gmail.com>
2025-12-09 12:15:06 +0530

    9th Dec - CRUD App Requirement

--- 10_sets/ops.py
+++ 10_sets/ops.py
@@ -46,3 +46,76 @@ data = {10,20,30,40,50}
 print(data)
 backup = data.copy()
 print(backup)
+
+# Set Specific Operations 
+s1 = {10,20,30,40,50}
+s2 = {40,50,60,70,80}
+
+# union(): combine both sets 
+print(s1.union(s2))
+print(s1 | s2)
+
+# intersection(): common elements from sets 
+print(s1.intersection(s2))
+print(s1 & s2)
+print(s1)
+print(s2)
+
+# intersection_update(): common elements from sets, updates calling set
+print(s1.intersection_update(s2))
+print(s1)
+print(s2)
+
+# difference(): remove common elements from set and give unique elements  
+s1 = {10,20,30,40,50}
+s2 = {40,50,60,70,80}
+print(s1.difference(s2))
+print(s1 - s2)
+s1 = {10,20,30,40,50}
+s2 = {40,50,60,70,80}
+print(s2.difference(s1))
+print(s1)
+print(s2)
+
+# difference_update(): same as difference(), but updates calling set 
+s1 = {10,20,30,40,50}
+s2 = {40,50,60,70,80}
+print(s1.difference_update(s2))
+print(s1)
+print(s2)
+
+# symmetric_difference(): removes common elements and takes combined elements from both sets 
+s1 = {10,20,30,40,50}
+s2 = {40,50,60,70,80}
+print(s1.symmetric_difference(s2))
+print(s1 ^ s2)
+print(s1)
+print(s2)
+
+# symmetric_difference_update(): same as symmetric_difference(), but updates calling set 
+s1 = {10,20,30,40,50}
+s2 = {40,50,60,70,80}
+print(s1.symmetric_difference_update(s2))
+print(s1)
+print(s2)
+
+# issubset(): checks if given set is subset of another set 
+s1 = {10,20,30,40,50}
+s2 = {40,50}
+s3 = {60,70,80}
+
+print(s1.issubset(s2))
+print(s2.issubset(s1))
+
+# issuperset(): checks if given set is superset of another set 
+s1 = {10,20,30,40,50}
+s2 = {40,50}
+s3 = {60,70,80}
+
+print(s1.issuperset(s2))
+print(s2.issuperset(s1))
+
+# isdisjoint(): checks if sets have common elements 
+print(s1.isdisjoint(s3))
+print(s2.isdisjoint(s3))
+print(s1.isdisjoint(s2))
---
+++ 11_crud_app/app.py
@@ -0,0 +1,22 @@
+# Student Management System
+
+# Menu Based System -> In Future once you learn full stack, replace with UI Elements like Buttons 
+
+# System Info - READ ONLY (Tuple)
+SYSTEM_INFO = ("Edify Technologies","Student Management System","v1")
+
+# Admin Info - READ ONLY (Tuple)
+ADMIN_INFO = ("909088080","admin@edify.com")
+
+# System Info On Startup
+print("=" * 50)
+print(f"Welcome To: {SYSTEM_INFO[0]}")
+print(f"Software Name: {SYSTEM_INFO[1]} - {SYSTEM_INFO[2]}")
+print("=" * 50)
+
+# Core Functionalities 
+# Add Student 
+# Student Data -> id, name, multiple duplicate scores, multiple unique skills 
+# Lists, Tuples, Dictionaries, Sets ?? -> 
+# id(Dict Key), name(Dict value), multiple duplicate scores(List), multiple unique skills(Sets) 
+# id, name, multiple duplicate scores, multiple unique skills 
